ed917399d31755116f0d574780dcd0df6c92c133
Alex Andrade <alex.andrade@7shifts.com>
2023-04-26 10:19:17 -0600

    Add Employee - Validating the form

--- src/employees/components/AddEmployeeModal/AddEmployeeModal.tsx
+++ src/employees/components/AddEmployeeModal/AddEmployeeModal.tsx
@@ -10,17 +10,33 @@ import {
     Stack,
     TextField
 } from '@7shifts/sous-chef';
+import { useFormik } from 'formik';
+import schema from './schema';
 
 type Props = {
     onClose: () => void;
 };
 
 const AddEmployeeModal = ({ onClose }: Props) => {
+    const formik = useFormik({
+        initialValues: {
+            firstName: '',
+            lastName: '',
+            email: '',
+            birthday: null
+        },
+        validationSchema: schema,
+        onSubmit: (values) => {
+            console.log('Will submit values');
+        }
+    });
+
     return (
         <Modal header="Add employee" onClose={onClose}>
             <Form
                 onSubmit={() => console.log('Will submit')}
                 stackContent={false}
+                formik={formik}
             >
                 <ModalBody>
                     <Stack>
@@ -38,13 +54,24 @@ const AddEmployeeModal = ({ onClose }: Props) => {
                         </FormRow>
                         <TextField name="email" label="Email" />
                         <FormRow columns={2}>
-                            <DateField name="birthday" label="Birthday" />
+                            <DateField
+                                name="birthday"
+                                label="Birthday"
+                                disabledDays={(day) => day > new Date()}
+                            />
                         </FormRow>
                     </Stack>
                 </ModalBody>
                 <ModalFooter
                     actions={{
-                        primary: <Button type="submit">Add employee</Button>,
+                        primary: (
+                            <Button
+                                type="submit"
+                                disabled={!formik.isValid || !formik.dirty}
+                            >
+                                Add employee
+                            </Button>
+                        ),
                         secondary: <Button onClick={onClose}>Close</Button>
                     }}
                 />
