dffe41add4eeb63562aa4824cd4f95fa31e23353
Alex Andrade <alex.andrade@7shifts.com>
2023-04-26 13:06:33 -0600

    Edit Employee - creating the basic edit form

--- src/employees/components/EditEmployeeForm/EditEmployeeForm.tsx
+++ src/employees/components/EditEmployeeForm/EditEmployeeForm.tsx
@@ -1,14 +1,54 @@
-import { Form, TextField } from '@7shifts/sous-chef';
+import {
+    Button,
+    DateField,
+    Form,
+    FormFooter,
+    FormRow,
+    TextField
+} from '@7shifts/sous-chef';
 import { Employee } from '../../types';
+import { useFormik } from 'formik';
+import schema, { FormValues } from './schema';
 
 type Props = {
     employee: Employee;
 };
 
 const EditEmployeeForm = ({ employee }: Props) => {
+    const formik = useFormik({
+        initialValues: {
+            firstName: '',
+            lastName: '',
+            email: '',
+            birthday: null
+        },
+        validationSchema: schema,
+        onSubmit: (values: FormValues, { setSubmitting }) => {
+            console.log('Will submit values', values);
+            setSubmitting(true);
+        }
+    });
+
     return (
-        <Form>
-            <TextField name="firstName" label="First name" />
+        <Form formik={formik}>
+            <FormRow>
+                <TextField name="firstName" label="First name" autoFocus />
+                <TextField name="lastName" label="Last name" />
+            </FormRow>
+            <TextField name="email" label="Email" />
+            <FormRow columns={2}>
+                <DateField
+                    name="birthday"
+                    label="Birthday"
+                    disabledDays={(day) => day > new Date()}
+                />
+            </FormRow>
+            <FormFooter
+                actions={{
+                    primary: <Button type="submit">Save</Button>,
+                    secondary: <Button>Cancel</Button>
+                }}
+            />
         </Form>
     );
 };
