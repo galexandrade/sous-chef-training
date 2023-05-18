e569d97e04f756e0d21f5c81b74e65f81018997c
Alex Andrade <alex.andrade@7shifts.com>
2023-05-18 09:03:24 -0600

    addressing next state

--- src/employees/components/EmployeesTable/EmployeesTable.tsx
+++ src/employees/components/EmployeesTable/EmployeesTable.tsx
@@ -1,4 +1,4 @@
-import { DataTable } from '@7shifts/sous-chef';
+import { DataTable, EmptyState } from '@7shifts/sous-chef';
 import { useEmployees } from '../../hooks/useEmployees';
 import { useUpdateCursor } from '../../hooks/useUpdateCursor';
 import EmployeeRow from './EmployeeRow/EmployeeRow';
@@ -8,6 +8,20 @@ type Props = {};
 const EmployeesTable = (props: Props) => {
     const { data, isLoading, cursor } = useEmployees();
     const updateCursor = useUpdateCursor();
+
+    if (!isLoading && data.length === 0) {
+        return (
+            <EmptyState
+                mediaUrl="/images/EmptyState.png"
+                size="small"
+                title="Setup your first location"
+            >
+                Get started by adding your first location to begin managing your
+                restaurant.
+            </EmptyState>
+        );
+    }
+
     return (
         <DataTable
             columns={[
@@ -18,11 +32,11 @@ const EmployeesTable = (props: Props) => {
             ]}
             items={data}
             isLoading={isLoading}
-            hasNext={!!cursor?.next}
             itemComponent={EmployeeRow}
-            hasPrevious={!!cursor?.prev}
-            onNextClick={() => updateCursor(cursor?.next)}
-            onPreviousClick={() => updateCursor(cursor?.prev)}
+            hasNext={!!cursor.next}
+            hasPrevious={!!cursor.prev}
+            onNextClick={() => updateCursor(cursor.next)}
+            onPreviousClick={() => updateCursor(cursor.prev)}
             showActionMenu
         />
     );
