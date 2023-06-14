0e670ed1e2276498ef59c06ca26b13fdd87549db
Alex Andrade <alex.andrade@7shifts.com>
2023-06-14 11:30:18 -0600

    creating the initial page

--- src/employees/pages/EmployeesPage.tsx
+++ src/employees/pages/EmployeesPage.tsx
@@ -1,9 +1,9 @@
-import { Text } from '@7shifts/sous-chef';
+import { Page } from '@7shifts/sous-chef';
 
 const EmployeesPage = () => {
     //This is for loading data into the page
     //const { isLoading, data, error, cursor } = useEmployees();
-    return <Text>Employees page will be here</Text>;
+    return <Page title="Employees">Page content here</Page>;
 };
 
 export default EmployeesPage;
