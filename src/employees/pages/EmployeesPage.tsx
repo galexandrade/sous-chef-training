957ead8e76781815e0ebbad7d4b517de7264a434
Alex Andrade <alex.andrade@7shifts.com>
2023-06-15 09:18:30 -0600

    banner

--- src/employees/pages/EmployeesPage.tsx
+++ src/employees/pages/EmployeesPage.tsx
@@ -1,11 +1,16 @@
 import { Page } from '@7shifts/sous-chef';
 import EmployeesPageActions from '../components/EmployeesPageActions';
+import EmployeesInviteBanner from '../components/EmployeesInviteBanner';
 
 const EmployeesPage = () => {
     //This is for loading data into the page
     //const { isLoading, data, error, cursor } = useEmployees();
     return (
-        <Page title="Employees" actions={<EmployeesPageActions />}>
+        <Page
+            title="Employees"
+            actions={<EmployeesPageActions />}
+            banner={<EmployeesInviteBanner />}
+        >
             Page content here
         </Page>
     );
