289357d54552a6bcd2f642c27db45738a2102350
Alex Andrade <alex.andrade@7shifts.com>
2023-11-21 14:24:04 -0600

    creating the edit employee route

---
+++ src/employees/pages/EditEmployeePage.tsx
@@ -0,0 +1,7 @@
+const EditEmployeePage = () => {
+    //This is for loading data into the page
+    //const { isLoading, data, error, cursor } = useEmployees();
+    return <div>edit employee page</div>;
+};
+
+export default EditEmployeePage;
