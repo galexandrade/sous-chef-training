abeccc081fb043dc1dbfee0b9aa581a7bd97f965
Alex Andrade <alex.andrade@7shifts.com>
2023-11-21 14:13:44 -0600

    adding pagination

--- src/employees/components/EmployeesTable/EmployeesTable.tsx
+++ src/employees/components/EmployeesTable/EmployeesTable.tsx
@@ -1,12 +1,14 @@
 import { DataTable } from '@7shifts/sous-chef';
 import { useEmployees } from '../../hooks/useEmployees';
 import EmployeeRow from './EmployeeRow';
+import { useSearchParams } from 'react-router-dom';
 
 type Props = {};
 
 const EmployeesTable = (props: Props) => {
     //This is for loading data into the page
-    const { isLoading, data } = useEmployees();
+    const { isLoading, data, cursor } = useEmployees();
+    const [queryParams, setQueryParam] = useSearchParams();
 
     return (
         <DataTable
@@ -20,6 +22,16 @@ const EmployeesTable = (props: Props) => {
             isLoading={isLoading}
             itemComponent={EmployeeRow}
             showActionMenu
+            hasNext={!!cursor?.next}
+            hasPrevious={!!cursor?.prev}
+            onNextClick={() => {
+                queryParams.set('cursor', cursor.next);
+                setQueryParam(queryParams);
+            }}
+            onPreviousClick={() => {
+                queryParams.set('cursor', cursor.prev);
+                setQueryParam(queryParams);
+            }}
         />
     );
 };
