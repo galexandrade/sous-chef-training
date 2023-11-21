3e8b3f2cd6d141f23ce1531c96dfcac8f20883c6
Alex Andrade <alex.andrade@7shifts.com>
2023-11-21 14:21:14 -0600

    filtering the table

--- src/employees/components/EmployeesPageFilters/EmployeesPageFilters.tsx
+++ src/employees/components/EmployeesPageFilters/EmployeesPageFilters.tsx
@@ -1,14 +1,31 @@
 import {
+    Button,
     IconMinusCircle,
     IconSearch,
     Inline,
-    SelectField,
-    TextField
+    SelectOption,
+    TextField,
+    ToolbarSelect
 } from '@7shifts/sous-chef';
 import { STATUSES_OPTIONS } from '../../constants';
 import './employees-page-filters.scss';
+import { useSearchField } from '../../hooks/useSearchField';
+import { useSearchParams } from 'react-router-dom';
+import { useRef } from 'react';
 
 const EmployeesPageFilters = () => {
+    const searchRef = useRef<HTMLInputElement>();
+    const { searchValue, setSearchValue } = useSearchField();
+    const [queryParams, setQueryParams] = useSearchParams();
+    const hasFilters = queryParams.has('search') || queryParams.has('status');
+
+    const handleResetFilters = () => {
+        setQueryParams({});
+        if (searchRef.current) {
+            searchRef.current.value = '';
+        }
+    };
+
     return (
         <Inline space={12}>
             <div className="employees-page-filters__search">
@@ -16,15 +33,31 @@ const EmployeesPageFilters = () => {
                     name="search"
                     placeholder="Search"
                     prefix={<IconSearch />}
+                    defaultValue={searchValue}
+                    onChange={setSearchValue}
+                    ref={searchRef}
                 />
             </div>
-            <SelectField
+            <ToolbarSelect
                 name="status"
                 placeholder="All statuses"
                 options={Object.values(STATUSES_OPTIONS)}
                 prefix={<IconMinusCircle />}
-                asToolbarFilter
+                value={
+                    STATUSES_OPTIONS[queryParams.get('status')] ||
+                    STATUSES_OPTIONS.ALL
+                }
+                onChange={(statusOption: SelectOption<string>) => {
+                    queryParams.set('status', statusOption.value);
+                    queryParams.delete('cursor');
+                    setQueryParams(queryParams);
+                }}
             />
+            {hasFilters && (
+                <Button theme="link-primary" onClick={handleResetFilters}>
+                    Reset filters
+                </Button>
+            )}
         </Inline>
     );
 };
