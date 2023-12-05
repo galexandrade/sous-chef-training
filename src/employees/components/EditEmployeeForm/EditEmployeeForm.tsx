4af628499fc32fcbd6e78c454e0674afcdd7f87d
Alex Andrade <alex.andrade@7shifts.com>
2023-12-05 13:47:07 -0600

    controling the form state for dynamic fields

--- src/employees/components/EditEmployeeForm/EditEmployeeForm.tsx
+++ src/employees/components/EditEmployeeForm/EditEmployeeForm.tsx
@@ -7,11 +7,12 @@ import {
     FormSection,
     IconTrash,
     Inline,
+    Stack,
     TextField
 } from '@7shifts/sous-chef';
 import { useNavigate } from 'react-router-dom';
 import { Employee } from '../../types';
-import { useFormik } from 'formik';
+import { FieldArray, FormikProvider, useFormik } from 'formik';
 import schema from './schema';
 
 type Props = {
@@ -33,52 +34,79 @@ const EditEmployeeForm = ({ employee }: Props) => {
         onSubmit: (values) => console.log('submit', values)
     });
     return (
-        <Form formik={formik}>
-            <FormSection title="Personal information">
-                <FormRow>
-                    <TextField name="firstName" label="First name" />
-                    <TextField name="lastName" label="Last name" />
-                </FormRow>
-                <TextField name="email" label="Email" />
-                <FormRow columns={2}>
-                    <DateField
-                        name="birthday"
-                        label="Birthday"
-                        disabledDays={(date) => date > new Date()}
+        <FormikProvider value={formik}>
+            <Form formik={formik}>
+                <FormSection title="Personal information">
+                    <FormRow>
+                        <TextField name="firstName" label="First name" />
+                        <TextField name="lastName" label="Last name" />
+                    </FormRow>
+                    <TextField name="email" label="Email" />
+                    <FormRow columns={2}>
+                        <DateField
+                            name="birthday"
+                            label="Birthday"
+                            disabledDays={(date) => date > new Date()}
+                        />
+                    </FormRow>
+                </FormSection>
+                <FormSection title="Contacts">
+                    <FieldArray
+                        name="contacts"
+                        render={({ push, remove }) => (
+                            <Stack>
+                                {formik.values.contacts.map((_, index) => (
+                                    <FormRow key={index}>
+                                        <TextField
+                                            name={`contacts[${index}].name`}
+                                            label={index === 0 && 'Name'}
+                                        />
+                                        <Inline flex={[1]} space={8}>
+                                            <TextField
+                                                name={`contacts[${index}].email`}
+                                                label={index === 0 && 'Email'}
+                                            />
+                                            <Button
+                                                theme="link-icon"
+                                                mt={index === 0 && 24}
+                                                onClick={() => remove(index)}
+                                            >
+                                                <IconTrash />
+                                            </Button>
+                                        </Inline>
+                                    </FormRow>
+                                ))}
+                                <Button
+                                    theme="link-primary"
+                                    onClick={() =>
+                                        push({ name: '', email: '' })
+                                    }
+                                >
+                                    Add contact
+                                </Button>
+                            </Stack>
+                        )}
                     />
-                </FormRow>
-            </FormSection>
-            <FormSection title="Contacts">
-                <FormRow>
-                    <TextField
-                        name="contacts[0].firstName"
-                        label="First name"
-                    />
-                    <Inline flex={[1]} space={8}>
-                        <TextField name="contacts[0].email" label="Email" />
-                        <Button theme="link-icon" mt={24}>
-                            <IconTrash />
-                        </Button>
-                    </Inline>
-                </FormRow>
-                <Button theme="link-primary">Add contact</Button>
-            </FormSection>
-            <FormFooter
-                actions={{
-                    primary: (
-                        <Button
-                            type="submit"
-                            disabled={!formik.isValid || !formik.dirty}
-                        >
-                            Save
-                        </Button>
-                    ),
-                    secondary: (
-                        <Button onClick={() => navigate('..')}>Cancel</Button>
-                    )
-                }}
-            />
-        </Form>
+                </FormSection>
+                <FormFooter
+                    actions={{
+                        primary: (
+                            <Button
+                                type="submit"
+                                disabled={!formik.isValid || !formik.dirty}
+                            >
+                                Save
+                            </Button>
+                        ),
+                        secondary: (
+                            <Button onClick={() => navigate('..')}>
+                                Cancel
+                            </Button>
+                        )
+                    }}
+                />
+            </Form>
+        </FormikProvider>
     );
 };
 
