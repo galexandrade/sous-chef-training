f51ed62e00807bef056132d69dfa63266aec3813
Alex Andrade <alex.andrade@7shifts.com>
2023-12-05 13:44:36 -0600

    dynamic fields layout

--- src/employees/components/EditEmployeeForm/EditEmployeeForm.tsx
+++ src/employees/components/EditEmployeeForm/EditEmployeeForm.tsx
@@ -4,6 +4,9 @@ import {
     Form,
     FormFooter,
     FormRow,
+    FormSection,
+    IconTrash,
+    Inline,
     TextField
 } from '@7shifts/sous-chef';
 import { useNavigate } from 'react-router-dom';
@@ -21,25 +24,45 @@ const EditEmployeeForm = ({ employee }: Props) => {
             firstName: employee.firstName,
             lastName: employee.lastName,
             email: employee.email,
-            birthday: new Date(employee.birthday)
+            birthday: new Date(employee.birthday),
+            contacts: [
+                { firstName: employee.firstName, lastName: employee.lastName }
+            ]
         },
         validationSchema: schema,
         onSubmit: (values) => console.log('submit', values)
     });
     return (
         <Form formik={formik}>
-            <FormRow>
-                <TextField name="firstName" label="First name" />
-                <TextField name="lastName" label="Last name" />
-            </FormRow>
-            <TextField name="email" label="Email" />
-            <FormRow columns={2}>
-                <DateField
-                    name="birthday"
-                    label="Birthday"
-                    disabledDays={(date) => date > new Date()}
-                />
-            </FormRow>
+            <FormSection title="Personal information">
+                <FormRow>
+                    <TextField name="firstName" label="First name" />
+                    <TextField name="lastName" label="Last name" />
+                </FormRow>
+                <TextField name="email" label="Email" />
+                <FormRow columns={2}>
+                    <DateField
+                        name="birthday"
+                        label="Birthday"
+                        disabledDays={(date) => date > new Date()}
+                    />
+                </FormRow>
+            </FormSection>
+            <FormSection title="Contacts">
+                <FormRow>
+                    <TextField
+                        name="contacts[0].firstName"
+                        label="First name"
+                    />
+                    <Inline flex={[1]} space={8}>
+                        <TextField name="contacts[0].email" label="Email" />
+                        <Button theme="link-icon" mt={24}>
+                            <IconTrash />
+                        </Button>
+                    </Inline>
+                </FormRow>
+                <Button theme="link-primary">Add contact</Button>
+            </FormSection>
             <FormFooter
                 actions={{
                     primary: (
