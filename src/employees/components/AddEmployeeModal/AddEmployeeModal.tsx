1a6fb965dc04e85e16522af02a060d43b4c68beb
Alex Andrade <alex.andrade@7shifts.com>
2023-12-19 11:45:27 -0600

    adding the form layout

--- src/employees/components/AddEmployeeModal/AddEmployeeModal.tsx
+++ src/employees/components/AddEmployeeModal/AddEmployeeModal.tsx
@@ -1,4 +1,15 @@
-import { Button, Modal, ModalBody, ModalFooter } from '@7shifts/sous-chef';
+import {
+    Button,
+    DateField,
+    Form,
+    FormRow,
+    InlineBanner,
+    Modal,
+    ModalBody,
+    ModalFooter,
+    Stack,
+    TextField
+} from '@7shifts/sous-chef';
 
 type Props = {
     onClose: () => void;
@@ -7,13 +18,34 @@ type Props = {
 const AddEmployeeModal = ({ onClose }: Props) => {
     return (
         <Modal header="Add employee" onClose={onClose} loading>
-            <ModalBody>Form will go here</ModalBody>
-            <ModalFooter
-                actions={{
-                    primary: <Button>Add employee</Button>,
-                    secondary: <Button onClick={onClose}>Cancel</Button>
-                }}
-            />
+            <Form>
+                <ModalBody>
+                    <Stack>
+                        <InlineBanner>
+                            You can complete the employee information after
+                            adding the new employee.
+                        </InlineBanner>
+                        <FormRow>
+                            <TextField name="firstName" label="First name" />
+                            <TextField name="lastName" label="Last name" />
+                        </FormRow>
+                        <TextField name="email" label="Email" />
+                        <FormRow columns={2}>
+                            <DateField
+                                name="birthday"
+                                label="Birthday"
+                                disabledDays={(date) => date > new Date()}
+                            />
+                        </FormRow>
+                    </Stack>
+                </ModalBody>
+                <ModalFooter
+                    actions={{
+                        primary: <Button type="submit">Add employee</Button>,
+                        secondary: <Button onClick={onClose}>Cancel</Button>
+                    }}
+                />
+            </Form>
         </Modal>
     );
 };
